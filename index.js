f5cf0bf86e18ea00d70f1803f4d921429aa4ced0
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-07-03 10:28:59 +0530

    add put req

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let courses = [
   { id: 1, name: "java" },
@@ -7,21 +8,30 @@ let courses = [
   { id: 3, name: "pyth" },
 ];
 
-// app.get("/", (req, res) => {
-//   res.json("Hello, Welcome");
-// });
+app.get("/", (req, res) => {
+  res.json("Hello, Welcome");
+});
 
 app.get("/courses", (req, res) => {
   res.json(courses);
 });
 
 app.post("/courses", (req, res) => {
-  //   console.log(req.body);
   const newCourse = req.body;
 
   const newId = courses.length ? courses[courses.length - 1].id + 1 : 1;
   const course = { id: newId, name: newCourse.name };
   courses.push(course);
+  res.send(courses);
+});
+
+app.put("/courses/:id", (req, res) => {
+  const courseId = parseInt(req.params.id);
+  const updatedCourse = req.body;
+
+  const course = courses.find((c) => c.id === courseId);
+  course.name = updatedCourse.name;
+  res.json(course);
 });
 
 app.listen(3000, () => {
