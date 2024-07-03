9a589a6fdb8ab5d47dc4666ca5ead34746293b0b
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2024-07-03 10:08:18 +0530

    add post req

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ let courses = [
   { id: 3, name: "pyth" },
 ];
 
+// app.get("/", (req, res) => {
+//   res.json("Hello, Welcome");
+// });
+
 app.get("/courses", (req, res) => {
   res.json(courses);
+});
+
+app.post("/courses", (req, res) => {
+  //   console.log(req.body);
+  const newCourse = req.body;
+
+  const newId = courses.length ? courses[courses.length - 1].id + 1 : 1;
+  const course = { id: newId, name: newCourse.name };
+  courses.push(course);
 });
 
 app.listen(3000, () => {
